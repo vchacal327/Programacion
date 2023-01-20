313658e99492359b1999a8501efcbbf62ab4f16c
vchacal327 <116277637+vchacal327@users.noreply.github.com>
2023-01-20 14:25:49 +0100

    Add files via upload

---
+++ src/UD4_EstructurasDeDatos/ArraysYMetodos.java
@@ -0,0 +1,18 @@
+package UD4_EstructurasDeDatos;
+
+public class ArraysYMetodos {
+    public static void m(int x) {
+        int result = x + 1;
+    }
+
+    public static void main(String[] args) {
+        int x = 1;
+        int valorInicial = x;
+        m(x);
+        if(x != valorInicial) {
+            System.out.println("El valor de x ha cambiado.");
+        } else {
+            System.out.println("El valor de x no ha cambiado.");
+        }
+    }
+}
---
+++ src/UD4_EstructurasDeDatos/EjemploArray.java
@@ -0,0 +1,12 @@
+public class EjemploArray {
+    public static void main(String[] args);
+        int [] a = {1, 2, 3, 4, 5};
+        a = new int(5);
+        System.out.println(a[4]);
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println(a.length);
+}
